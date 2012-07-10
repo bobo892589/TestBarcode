a2d07dcfebba6b04647a2aec9aa79048d8dfc2a8
bobo <bobo@bobomatoMacBook-Pro.local>
2012-07-10 11:03:11 +0800

    Initial Commit

---
+++ TestBarcodeTests/TestBarcodeTests.h
@@ -0,0 +1,13 @@
+//
+//  TestBarcodeTests.h
+//  TestBarcodeTests
+//
+//  Created by bobo on 12-7-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestBarcodeTests : SenTestCase
+
+@end
---
+++ TestBarcodeTests/TestBarcodeTests.m
@@ -0,0 +1,32 @@
+//
+//  TestBarcodeTests.m
+//  TestBarcodeTests
+//
+//  Created by bobo on 12-7-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestBarcodeTests.h"
+
+@implementation TestBarcodeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestBarcodeTests");
+}
+
+@end
