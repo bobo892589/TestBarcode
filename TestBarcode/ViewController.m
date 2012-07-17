cac5e093fe50f235316daa0d5052233292097a40
bobo <540844924@qq.com>
2012-07-17 11:49:36 +0800

    add test for create.

--- TestBarcode/ViewController.m
+++ TestBarcode/ViewController.m
@@ -7,21 +7,29 @@
 //
 
 #import "ViewController.h"
+#import "NKDCode128Barcode.h"
+#import "UIImage-NKDBarcode.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize barcodeImageView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NKDCode128Barcode *barcode = [[NKDCode128Barcode alloc] initWithContent:@"2222"];
+    UIImage *image = [UIImage imageFromBarcode:barcode];
+    self.barcodeImageView.image = image;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
+    [self setBarcodeImageView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +39,8 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (void)dealloc {
+    [barcodeImageView release];
+    [super dealloc];
+}
 @end
